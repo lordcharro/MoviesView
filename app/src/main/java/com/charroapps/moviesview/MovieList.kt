93e4c836ff55bc499b29714315108f14d69263fc
Ivo Lopes <charro.il@gmail.com>
2018-06-06 22:57:32 +0200

    Initial commit

---
+++ app/src/main/java/com/charroapps/moviesview/MovieList.kt
@@ -0,0 +1,12 @@
+package com.charroapps.moviesview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_list)
+    }
+}
