cd5c9992e3db79aa121325bc865e99357f790b77
Ivo Lopes <charro.il@gmail.com>
2018-06-11 17:19:17 +0200

    add retrofit connection

--- app/src/main/java/com/charroapps/moviesview/Network/GetDataService.kt
+++ app/src/main/java/com/charroapps/moviesview/Network/GetDataService.kt
@@ -1,13 +1,19 @@
 package com.charroapps.moviesview.Network
 
-import com.charroapps.moviesview.Models.Movie
+import com.charroapps.moviesview.Models.Result
 import retrofit2.Call
 import retrofit2.http.GET
-
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 
 interface GetDataService {
 
-    @GET("/photos")
-    fun getAllPhotos(): Call<List<Movie>>
+    //https://api.themoviedb.org/3/search/movie?api_key=9101601cde657ddff84db932cb019d8a&query=keyword
+    @GET("/3/search/movie")
+    fun getMovies(@Query("api_key") api_key : String,
+                  @Query("query") movie : String): Call<Result>
+
+    @GET("/3/search/movie?api_key=9101601cde657ddff84db932cb019d8a&query=scary")
+    fun getMovies2(): Call<Result>
 }
